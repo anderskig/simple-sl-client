0417c639ff0e75709bb132a9be7c5213e5406d89
Anders Gustafsson <anders@photowall.se>
2020-02-21 20:09:08 +0100

    Updates

---
+++ precache-manifest.e3da5781f6fefacd168bf22f10455ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9faf9253825571505b30cb885fba529",
+    "url": "/simple-sl-client/index.html"
+  },
+  {
+    "revision": "a3f549da5aecb3453b35",
+    "url": "/simple-sl-client/static/css/main.e642e215.chunk.css"
+  },
+  {
+    "revision": "17c9d64d2d7c20cac2f4",
+    "url": "/simple-sl-client/static/js/2.5638b143.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/simple-sl-client/static/js/2.5638b143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f549da5aecb3453b35",
+    "url": "/simple-sl-client/static/js/main.f7cad8a5.chunk.js"
+  },
+  {
+    "revision": "8f2986dce7fa9b466c87",
+    "url": "/simple-sl-client/static/js/runtime-main.cb0e94ed.js"
+  }
+]);
